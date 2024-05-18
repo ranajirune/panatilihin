7bab8c0d0f908b5be549ec5196cd8e4674e29f51
rynel <ranajirune@gmail.com>
2024-05-18 22:23:50 +0800

    Added deleteNote(Long) method

--- app/src/main/java/com/raineru/panatilihin/data/OfflineNotesRepository.kt
+++ app/src/main/java/com/raineru/panatilihin/data/OfflineNotesRepository.kt
@@ -6,11 +6,13 @@ import javax.inject.Inject
 class OfflineNotesRepository @Inject constructor(private val noteDao: NoteDao) : NotesRepository {
     override fun getAllNotes(): Flow<List<Note>> = noteDao.getAllNotes()
 
-    override fun getNote(id: Int): Flow<Note?> = noteDao.getNote(id)
+    override fun getNote(id: Long): Flow<Note?> = noteDao.getNote(id)
 
-    override suspend fun insertNote(item: Note) = noteDao.insert(item)
+    override suspend fun insertNote(item: Note): Long = noteDao.insert(item)
 
     override suspend fun deleteNote(item: Note) = noteDao.delete(item)
+
+    override suspend fun deleteNote(id: Long) = noteDao.delete(id)
 
     override suspend fun updateNote(item: Note) = noteDao.update(item)
 }
