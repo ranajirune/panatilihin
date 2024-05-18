958d2328f241b8d77b5823e10388bdb32fefb955
rynel <ranajirune@gmail.com>
2024-05-18 22:18:41 +0800

    - Changed getNote() parameter to Long - Added Long return type to insertNote for the id of the note inserted - Added deleteNote(id: Long) overload

--- app/src/main/java/com/raineru/panatilihin/data/NotesRepository.kt
+++ app/src/main/java/com/raineru/panatilihin/data/NotesRepository.kt
@@ -5,11 +5,13 @@ import kotlinx.coroutines.flow.Flow
 interface NotesRepository {
     fun getAllNotes(): Flow<List<Note>>
 
-    fun getNote(id: Int): Flow<Note?>
+    fun getNote(id: Long): Flow<Note?>
 
-    suspend fun insertNote(item: Note)
+    suspend fun insertNote(item: Note): Long
 
     suspend fun deleteNote(item: Note)
 
     suspend fun updateNote(item: Note)
+
+    suspend fun deleteNote(id: Long)
 }
