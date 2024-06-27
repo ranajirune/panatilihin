3656e6ab79642c6fa100e12ce50749f19985834a
rynel <ranajirune@gmail.com>
2024-06-28 02:32:40 +0800

    Updated EditNoteViewModel other member variables to use State instead of Flow.

--- app/src/main/java/com/raineru/panatilihin/ui/EditNoteViewModel.kt
+++ app/src/main/java/com/raineru/panatilihin/ui/EditNoteViewModel.kt
@@ -1,6 +1,7 @@
 package com.raineru.panatilihin.ui
 
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
@@ -10,16 +11,12 @@ import com.raineru.panatilihin.data.Note
 import com.raineru.panatilihin.data.NotesRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.take
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -39,27 +36,29 @@ class EditNoteViewModel @Inject constructor(
 
     private val contentFlow = snapshotFlow { content }
 
-    // Prevent updating title and content if content not yet loaded
-    private val _contentLoaded = MutableStateFlow(false)
-    val contentLoaded: StateFlow<Boolean> = _contentLoaded.asStateFlow()
+    var contentLoaded by mutableStateOf(false)
+        private set
 
-    private val noteId: MutableStateFlow<Long> = MutableStateFlow(0)
+    private val contentLoadedFlow = snapshotFlow { contentLoaded }
+
+    private var noteId by mutableLongStateOf(0)
+    private val noteIdFlow = snapshotFlow { noteId }
 
     fun updateTitle(title: String) {
-        if (contentLoaded.value) {
+        if (contentLoaded) {
             this.title = title
         }
     }
 
     fun updateContent(content: String) {
-        if (contentLoaded.value) {
+        if (contentLoaded) {
             this.content = content
         }
     }
 
-    fun setNoteId(noteId: Long) {
-        if (!contentLoaded.value) {
-            this.noteId.value = noteId
+    fun updateNoteId(noteId: Long) {
+        if (!contentLoaded) {
+            this.noteId = noteId
         }
     }
 
@@ -72,7 +71,7 @@ class EditNoteViewModel @Inject constructor(
     init {
         // Update title and content from repo when noteId changes
         viewModelScope.launch {
-            noteId.filter { it != 0L }
+            noteIdFlow.filter { it != 0L }
                 .take(1)
                 .collectLatest { noteId ->
                     repository.getNote(noteId)
@@ -82,10 +81,7 @@ class EditNoteViewModel @Inject constructor(
                             println("fetchedNote: $fetchedNote")
                             title = fetchedNote.title
                             content = fetchedNote.content
-                            _contentLoaded.update {
-                                println("_contentLoaded: true")
-                                true
-                            }
+                            contentLoaded = true
                         }
                 }
         }
@@ -93,10 +89,10 @@ class EditNoteViewModel @Inject constructor(
         // Observe changes to title and content and update note in repo
         viewModelScope.launch {
             // If contentLoaded is true, noteId is already set
-            contentLoaded
+            contentLoadedFlow
                 // Filter if content not yet loaded
                 .filter { it }
-                .combine(noteId) { _, noteId ->
+                .combine(noteIdFlow) { _, noteId ->
                     noteId
                 }
                 .combine(titleFlow) { noteId, title ->
