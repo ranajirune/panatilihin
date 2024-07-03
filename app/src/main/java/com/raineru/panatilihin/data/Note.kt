1cfed5c074ddfa6337b770e4c57c9879a36f32a1
rynel <ranajirune@gmail.com>
2024-07-04 00:29:28 +0800

    Added NOteLabels data class.

--- app/src/main/java/com/raineru/panatilihin/data/Note.kt
+++ app/src/main/java/com/raineru/panatilihin/data/Note.kt
@@ -1,7 +1,10 @@
 package com.raineru.panatilihin.data
 
+import androidx.room.Embedded
 import androidx.room.Entity
+import androidx.room.Junction
 import androidx.room.PrimaryKey
+import androidx.room.Relation
 
 @Entity(tableName = "notes")
 data class Note(
@@ -12,3 +15,20 @@ data class Note(
     @PrimaryKey(autoGenerate = true)
     var id: Long = 0
 )
+
+data class NoteLabels(
+    @Embedded
+    val note: Note,
+
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "id",
+        associateBy = Junction(
+            value = NoteAndLabelCrossRef::class,
+            parentColumn = "noteId",
+            entityColumn = "labelId"
+        )
+    )
+    val labels: List<Label>
+)
+
