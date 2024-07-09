a251a1b9be1315d393ad75197dfcd8277138c08f
rynel <ranajirune@gmail.com>
2024-07-09 17:42:37 +0800

    Created EditNoteLabelScreen stateless and stateful overload

--- app/src/main/java/com/raineru/panatilihin/ui/EditNoteLabelScreen.kt
+++ app/src/main/java/com/raineru/panatilihin/ui/EditNoteLabelScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
@@ -29,66 +30,137 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.raineru.panatilihin.data.Label
 import com.raineru.panatilihin.ui.theme.PanatilihinTheme
+import com.raineru.panatilihin.ui.viewmodel.EditNoteLabelViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EditNoteLabelScreen(
-    labels: List<Label>,
+    allLabel: List<Label>,
+    selectedLabels: List<Long>,
     query: String,
     onUpdateLabel: (Long, Boolean) -> Unit,
     onUpdateQuery: (String) -> Unit,
     onCreateNewLabel: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    LazyColumn(modifier = modifier) {
-        if (
-            labels.count { it.name.contentEquals(query.trim(), ignoreCase = true) } == 0 &&
-            query.isNotBlank()
-        ) {
-            item(key = 0) {
-                CreateNewLabel(
-                    text = query.trim(),
-                    onClick = {
-                        onCreateNewLabel(it)
-                        onUpdateQuery("")
-                    }
-                )
-            }
+    SearchBar(
+        query = query,
+        onQueryChange = onUpdateQuery,
+        onSearch = {},
+        active = true,
+        onActiveChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = "Search"
+            )
+        },
+        placeholder = {
+            Text("Enter label name")
         }
+    ) {
+        LazyColumn(modifier = modifier) {
+            if (
+                allLabel.count { it.name.contentEquals(query.trim(), ignoreCase = true) } == 0 &&
+                query.isNotBlank()
+            ) {
+                item(key = 0) {
+                    CreateNewLabel(
+                        text = query.trim(),
+                        onClick = {
+                            onCreateNewLabel(it)
+                            onUpdateQuery("")
+                        }
+                    )
+                }
+            }
 
-        labels
-            .filter {
-                it.name.contains(query.trim(), ignoreCase = true)
-            }
-            .forEach { label ->
-            item(key = label.id) {
-                ToggleableLabel(
-                    text = label.name,
-                    onCheckedChange = {
-                        onUpdateLabel(label.id, it)
+            allLabel
+                .filter {
+                    it.name.contains(query.trim(), ignoreCase = true)
+                }
+                .forEach { label ->
+                    item(key = label.id) {
+                        ToggleableLabel(
+                            checked = selectedLabels.contains(label.id),
+                            text = label.name,
+                            onCheckedChange = {
+                                onUpdateLabel(label.id, it)
+                            }
+                        )
                     }
-                )
-            }
+                }
         }
     }
+}
+
+@Composable
+fun EditNoteLabelScreen(
+    noteId: Long,
+    editNoteLabelViewModel: EditNoteLabelViewModel = hiltViewModel()
+) {
+    var query by rememberSaveable {
+        mutableStateOf("")
+    }
+
+    val allLabel by editNoteLabelViewModel.allLabels.collectAsStateWithLifecycle(
+        lifecycleOwner = androidx.compose.ui.platform.LocalLifecycleOwner.current
+    )
+
+    val noteLabels by editNoteLabelViewModel.noteLabels.collectAsStateWithLifecycle(
+        lifecycleOwner = androidx.compose.ui.platform.LocalLifecycleOwner.current
+    )
+
+    LaunchedEffect(Unit) {
+        editNoteLabelViewModel.updateNoteId(noteId)
+    }
+
+    EditNoteLabelScreen(
+        allLabel = allLabel,
+        onUpdateLabel = { id: Long, checked: Boolean ->
+            editNoteLabelViewModel.updateNoteLabel(id, checked)
+        },
+        query = query,
+        onUpdateQuery = {
+            query = it
+        },
+        onCreateNewLabel = {
+            editNoteLabelViewModel.createLabel(name = it)
+        },
+        selectedLabels = noteLabels.map { it.id }
+    )
 }
 
 @Composable
 fun ToggleableLabel(
     text: String,
     onCheckedChange: (Boolean) -> Unit,
+    checked: Boolean,
     modifier: Modifier = Modifier
 ) {
-    var checked by rememberSaveable {
-        mutableStateOf(false)
-    }
+    ToggleableLabelContent(
+        text = text,
+        onCheckedChange = onCheckedChange,
+        checked = checked,
+        modifier = modifier
+    )
+}
 
+@Composable
+private fun ToggleableLabelContent(
+    text: String,
+    onCheckedChange: (Boolean) -> Unit,
+    checked: Boolean,
+    modifier: Modifier = Modifier
+) {
     Row(
         modifier = modifier
             .clickable {
-                checked = !checked
-                onCheckedChange.invoke(checked)
+                onCheckedChange.invoke(!checked)
             }
             .fillMaxWidth()
             .height(54.dp),
@@ -105,11 +177,31 @@ fun ToggleableLabel(
         Checkbox(
             checked = checked,
             onCheckedChange = {
-                checked = it
                 onCheckedChange.invoke(it)
             },
         )
     }
+}
+
+@Composable
+fun ToggleableLabel(
+    text: String,
+    onCheckedChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    var checked by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    ToggleableLabelContent(
+        text = text,
+        onCheckedChange = {
+            checked = it
+            onCheckedChange(it)
+        },
+        checked = checked,
+        modifier = modifier
+    )
 }
 
 @Composable
@@ -157,57 +249,44 @@ fun ToggleableLabelPreview() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
 fun EditNoteLabelScreenPreview() {
     PanatilihinTheme {
-        var query by rememberSaveable {
+        val selectedLabels = remember {
+            mutableStateListOf(
+                1L, 3L, 5L
+            )
+        }
+
+        val allLabels = listOf(
+            Label(name = "Label 1", id = 1L),
+            Label(name = "Label 2", id = 2L),
+            Label(name = "Label 3", id = 3L),
+            Label(name = "Label 4", id = 4L),
+            Label(name = "Label 5", id = 5L),
+        )
+
+        var query by remember {
             mutableStateOf("")
         }
-        val list = remember {
-            mutableStateListOf(
-                Label(name = "Bulbasaur", id = 1L),
-                Label(name = "Ivysaur", id = 2L),
-                Label(name = "Venusaur", id = 3L),
-                Label(name = "Charmander", id = 4L),
-                Label(name = "Charmeleon", id = 5L),
-                Label(name = "Charizard", id = 6L),
-                Label(name = "Squirtle", id = 7L),
-                Label(name = "Wartortle", id = 8L),
-                Label(name = "Blastoise", id = 9L),
-            )
-        }
-        SearchBar(
-            query = query,
-            onQueryChange = { query = it },
-            onSearch = {},
-            active = true,
-            onActiveChange = {},
-            leadingIcon = {
-                Icon(
-                    imageVector = Icons.Default.Search,
-                    contentDescription = "Search"
-                )
-            },
-            placeholder = {
-                Text("Enter label name")
-            }
-        ) {
-            EditNoteLabelScreen(
-                labels = list,
-                onUpdateLabel = { id: Long, checked: Boolean ->
-                    Log.d("EditNoteLabelScreen", "onUpdateLabel: $id, $checked")
-                },
-                query = query,
-                onUpdateQuery = {
-                    query = it
-                },
-                onCreateNewLabel = {
-                    list.add(0, Label(name = it, id = System.currentTimeMillis()))
+
+        EditNoteLabelScreen(
+            allLabel = allLabels,
+            onUpdateLabel = { id, checked ->
+                if (checked) {
+                    selectedLabels.add(id)
+                } else {
+                    selectedLabels.remove(id)
                 }
-            )
-        }
+            },
+            query = query,
+            onUpdateQuery = {
+                query = it
+            },
+            onCreateNewLabel = {},
+            selectedLabels = selectedLabels
+        )
     }
 }
 
@@ -216,5 +295,38 @@ fun EditNoteLabelScreenPreview() {
 fun CreateNewLabelPreview() {
     PanatilihinTheme {
         CreateNewLabel(text = "Label S", onClick = {})
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HoistedToggleableLabelPreview() {
+    PanatilihinTheme {
+        val text = "Bulbasaur"
+        var checked by remember {
+            mutableStateOf(false)
+        }
+        ToggleableLabel(
+            text = text,
+            onCheckedChange = {
+                checked = it
+                Log.d("EditNoteLabelScreen", "onCheckedChange: $it")
+            },
+            checked = checked
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NotHoistedToggleableLabelPreview() {
+    PanatilihinTheme {
+        val text = "Bulbasaur"
+        ToggleableLabel(
+            text = text,
+            onCheckedChange = {
+                Log.d("EditNoteLabelScreen", "onCheckedChange: $it")
+            }
+        )
     }
 }
