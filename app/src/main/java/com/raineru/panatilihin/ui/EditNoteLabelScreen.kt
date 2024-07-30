5482c76cfa48a404c18b785b677f22bd6bc4b88f
rynel <ranajirune@gmail.com>
2024-07-30 21:28:41 +0800

    Added EditNoteLabelScreen and refactored some codes.

--- app/src/main/java/com/raineru/panatilihin/ui/EditNoteLabelScreen.kt
+++ app/src/main/java/com/raineru/panatilihin/ui/EditNoteLabelScreen.kt
@@ -3,21 +3,33 @@ package com.raineru.panatilihin.ui
 import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.MailOutline
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.SearchBar
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -28,6 +40,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -39,75 +52,58 @@ import com.raineru.panatilihin.ui.viewmodel.EditNoteLabelViewModel
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EditNoteLabelScreen(
-    allLabel: List<Label>,
-    selectedLabels: List<Long>,
     query: String,
+    onQueryChange: (String) -> Unit,
+    onBackClick: () -> Unit,
+    allLabels: List<Label>,
+    selectedLabels: List<Long>,
     onUpdateLabel: (Long, Boolean) -> Unit,
-    onUpdateQuery: (String) -> Unit,
     onCreateNewLabel: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    SearchBar(
-        query = query,
-        onQueryChange = onUpdateQuery,
-        onSearch = {},
-        active = true,
-        onActiveChange = {},
-        leadingIcon = {
-            Icon(
-                imageVector = Icons.Default.Search,
-                contentDescription = "Search"
-            )
-        },
-        placeholder = {
-            Text("Enter label name")
-        }
-    ) {
-        LazyColumn(modifier = modifier) {
-            if (
-                allLabel.count { it.name.contentEquals(query.trim(), ignoreCase = true) } == 0 &&
-                query.isNotBlank()
-            ) {
-                item(key = 0) {
-                    CreateNewLabel(
-                        text = query.trim(),
-                        onClick = {
-                            onCreateNewLabel(it)
-                            onUpdateQuery("")
-                        }
-                    )
-                }
-            }
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
-            allLabel
-                .filter {
-                    it.name.contains(query.trim(), ignoreCase = true)
-                }
-                .forEach { label ->
-                    item(key = label.id) {
-                        ToggleableLabel(
-                            checked = selectedLabels.contains(label.id),
-                            text = label.name,
-                            onCheckedChange = {
-                                onUpdateLabel(label.id, it)
-                            }
-                        )
-                    }
-                }
+    Scaffold(
+        modifier = Modifier
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBar(
+                title = {
+                    LabelSearchBar(
+                        text = query,
+                        onTextChange = onQueryChange,
+                        onBackClick = onBackClick
+                    )
+                },
+                scrollBehavior = scrollBehavior
+            )
         }
+    ) { padding ->
+        EditNoteLabelList(
+            allLabels = allLabels,
+            selectedLabels = selectedLabels,
+            query = query,
+            onUpdateLabel = onUpdateLabel,
+            onUpdateQuery = onQueryChange,
+            onCreateNewLabel = onCreateNewLabel,
+            contentPadding = padding,
+            modifier = modifier
+        )
     }
 }
 
 @Composable
 fun EditNoteLabelScreen(
     noteId: Long,
+    onBackClick: () -> Unit,
+    modifier: Modifier = Modifier,
     editNoteLabelViewModel: EditNoteLabelViewModel = hiltViewModel()
 ) {
     var query by rememberSaveable {
         mutableStateOf("")
     }
 
-    val allLabel by editNoteLabelViewModel.allLabels.collectAsStateWithLifecycle()
+    val allLabels by editNoteLabelViewModel.allLabels.collectAsStateWithLifecycle()
     val noteLabels by editNoteLabelViewModel.noteLabels.collectAsStateWithLifecycle()
 
     LaunchedEffect(Unit) {
@@ -115,23 +111,90 @@ fun EditNoteLabelScreen(
     }
 
     EditNoteLabelScreen(
-        allLabel = allLabel,
+        allLabels = allLabels,
         onUpdateLabel = { id: Long, checked: Boolean ->
             editNoteLabelViewModel.updateNoteLabel(id, checked)
         },
         query = query,
-        onUpdateQuery = {
-            query = it
-        },
         onCreateNewLabel = {
             editNoteLabelViewModel.createLabel(name = it)
         },
-        selectedLabels = noteLabels.map { it.id }
+        selectedLabels = noteLabels.map { it.id },
+        modifier = modifier,
+        onBackClick = onBackClick,
+        onQueryChange = {
+            query = it
+        }
     )
 }
 
 @Composable
-fun ToggleableLabel(
+fun EditNoteLabelList(
+    allLabels: List<Label>,
+    selectedLabels: List<Long>,
+    query: String,
+    onUpdateLabel: (Long, Boolean) -> Unit,
+    onUpdateQuery: (String) -> Unit,
+    onCreateNewLabel: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp)
+) {
+    LazyColumn(
+        modifier = modifier
+            .fillMaxSize()
+            .imePadding(),
+        contentPadding = PaddingValues(
+            top = contentPadding.calculateTopPadding()
+        )
+    ) {
+        if (
+            allLabels.count {
+                it.name.contentEquals(
+                    query.trim(),
+                    ignoreCase = true
+                )
+            } == 0 &&
+            query.isNotBlank()
+        ) {
+            item(key = 0) {
+                CreateNewLabel(
+                    text = query.trim(),
+                    onClick = {
+                        onCreateNewLabel(it)
+                        onUpdateQuery("")
+                    }
+                )
+            }
+        }
+
+        allLabels
+            .filter {
+                it.name.contains(query.trim(), ignoreCase = true)
+            }
+            .forEach { label ->
+                item(key = label.id) {
+                    ToggleableLabel(
+                        checked = selectedLabels.contains(label.id),
+                        text = label.name,
+                        onCheckedChange = {
+                            onUpdateLabel(label.id, it)
+                        }
+                    )
+                }
+            }
+
+        item {
+            Spacer(
+                modifier = Modifier.windowInsetsBottomHeight(
+                    WindowInsets.systemBars
+                )
+            )
+        }
+    }
+}
+
+@Composable
+private fun ToggleableLabel(
     text: String,
     onCheckedChange: (Boolean) -> Unit,
     checked: Boolean,
@@ -179,7 +242,7 @@ private fun ToggleableLabelContent(
 }
 
 @Composable
-fun ToggleableLabel(
+private fun ToggleableLabel(
     text: String,
     onCheckedChange: (Boolean) -> Unit,
     modifier: Modifier = Modifier,
@@ -200,7 +263,7 @@ fun ToggleableLabel(
 }
 
 @Composable
-fun CreateNewLabel(
+private fun CreateNewLabel(
     text: String,
     onClick: (String) -> Unit,
     modifier: Modifier = Modifier
@@ -229,7 +292,7 @@ fun CreateNewLabel(
     showBackground = true
 )
 @Composable
-fun ToggleableLabelPreview() {
+private fun ToggleableLabelPreview() {
     PanatilihinTheme {
         val text by rememberSaveable {
             mutableStateOf("A New Label")
@@ -246,7 +309,7 @@ fun ToggleableLabelPreview() {
 
 @Preview(showBackground = true)
 @Composable
-fun EditNoteLabelScreenPreview() {
+private fun EditNoteLabelScreenPreview() {
     PanatilihinTheme {
         val selectedLabels = remember {
             mutableStateListOf(
@@ -266,8 +329,8 @@ fun EditNoteLabelScreenPreview() {
             mutableStateOf("")
         }
 
-        EditNoteLabelScreen(
-            allLabel = allLabels,
+        EditNoteLabelList(
+            allLabels = allLabels,
             onUpdateLabel = { id, checked ->
                 if (checked) {
                     selectedLabels.add(id)
@@ -280,14 +343,14 @@ fun EditNoteLabelScreenPreview() {
                 query = it
             },
             onCreateNewLabel = {},
-            selectedLabels = selectedLabels
+            selectedLabels = selectedLabels,
         )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun CreateNewLabelPreview() {
+private fun CreateNewLabelPreview() {
     PanatilihinTheme {
         CreateNewLabel(text = "Label S", onClick = {})
     }
@@ -295,7 +358,7 @@ fun CreateNewLabelPreview() {
 
 @Preview(showBackground = true)
 @Composable
-fun HoistedToggleableLabelPreview() {
+private fun HoistedToggleableLabelPreview() {
     PanatilihinTheme {
         val text = "Bulbasaur"
         var checked by remember {
@@ -314,13 +377,54 @@ fun HoistedToggleableLabelPreview() {
 
 @Preview(showBackground = true)
 @Composable
-fun NotHoistedToggleableLabelPreview() {
+private fun NotHoistedToggleableLabelPreview() {
     PanatilihinTheme {
         val text = "Bulbasaur"
         ToggleableLabel(
             text = text,
             onCheckedChange = {
                 Log.d("EditNoteLabelScreen", "onCheckedChange: $it")
+            }
+        )
+    }
+}
+
+@Composable
+private fun LabelSearchBar(
+    text: String,
+    onTextChange: (String) -> Unit,
+    onBackClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(54.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        IconButton(onClick = onBackClick) {
+            Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null)
+        }
+        Spacer(modifier = Modifier.width(12.dp))
+        BasicTextField(
+            value = text,
+            textStyle = MaterialTheme.typography.labelLarge,
+            onValueChange = onTextChange,
+            modifier = Modifier.weight(1f),
+            singleLine = true,
+            decorationBox = {
+                Box(
+                    contentAlignment = Alignment.CenterStart
+                ) {
+                    if (text.isEmpty()) {
+                        Text(
+                            "Enter label name",
+                            style = MaterialTheme.typography.labelLarge,
+                            color = MaterialTheme.colorScheme.onTertiary
+                        )
+                    }
+                    it()
+                }
             }
         )
     }
