d37d7009e0bb999ce4d72f21c6015c3ea1b31d6c
rynel <ranajirune@gmail.com>
2024-09-05 15:10:28 +0800

    Added scrollBehavior on editDraftNoteLabelScreenNavigationRoute composable

--- app/src/main/java/com/raineru/panatilihin/PanatilihinNavGraph.kt
+++ app/src/main/java/com/raineru/panatilihin/PanatilihinNavGraph.kt
@@ -1,5 +1,8 @@
 package com.raineru.panatilihin
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -7,6 +10,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewModelScope
@@ -27,12 +31,14 @@ import com.raineru.panatilihin.navigation.navigateToEditNoteLabelScreen
 import com.raineru.panatilihin.navigation.navigateToEditNoteScreen
 import com.raineru.panatilihin.navigation.noteIdParameter
 import com.raineru.panatilihin.ui.CreateNoteScreen
+import com.raineru.panatilihin.ui.EditNoteLabelList
 import com.raineru.panatilihin.ui.EditNoteLabelScreen
 import com.raineru.panatilihin.ui.EditNoteScreen
 import com.raineru.panatilihin.ui.HomeScreen
 import com.raineru.panatilihin.ui.viewmodel.HomeScreenViewModel
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PanatilihinNavGraph(
     modifier: Modifier = Modifier,
@@ -123,19 +129,27 @@ fun PanatilihinNavGraph(
                 mutableStateOf("")
             }
 
-            EditNoteLabelScreen(
-                allLabel = allLabels,
-                selectedLabels = draftNoteLabels,
-                query = query,
-                onUpdateLabel = homeScreenViewModel::updateDraftNoteLabel,
-                onUpdateQuery = { query = it },
-                onCreateNewLabel = {
-                    homeScreenViewModel.viewModelScope.launch {
-                        val id = homeScreenViewModel.createLabel(Label(it))
-                        homeScreenViewModel.updateDraftNoteLabel(id, true)
-                    }
-                },
-                onBackClick = { navController.navigateUp() })
+            val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+            Scaffold(
+                modifier = Modifier
+                    .nestedScroll(scrollBehavior.nestedScrollConnection)
+            ) { padding ->
+                EditNoteLabelList(
+                    allLabels = allLabels,
+                    selectedLabels = draftNoteLabels,
+                    query = query,
+                    onUpdateLabel = homeScreenViewModel::updateDraftNoteLabel,
+                    onUpdateQuery = { query = it },
+                    onCreateNewLabel = {
+                        homeScreenViewModel.viewModelScope.launch {
+                            val id = homeScreenViewModel.createLabel(Label(it))
+                            homeScreenViewModel.updateDraftNoteLabel(id, true)
+                        }
+                    },
+                    contentPadding = padding
+                )
+            }
         }
     }
 }
