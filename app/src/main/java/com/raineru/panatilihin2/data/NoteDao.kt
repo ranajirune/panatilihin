51aa0b541b7212276215f04aa6f1dd64767e60da
rynel <ranajirune@gmail.com>
2024-10-15 02:32:26 +0800

    Added NoteDao, used to write and read note related info from the database.

---
+++ app/src/main/java/com/raineru/panatilihin2/data/NoteDao.kt
@@ -0,0 +1,25 @@
+package com.raineru.panatilihin2.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: Note)
+
+    @Update
+    suspend fun updateNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+    @Query("SELECT * FROM notes WHERE id = :id")
+    fun getNote(id: Long): Flow<Note?>
+}
