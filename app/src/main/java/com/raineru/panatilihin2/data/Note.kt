bfac902b0a5d41a6afbd8a68c481fa5a92e1d2bc
rynel <ranajirune@gmail.com>
2024-10-14 16:31:52 +0800

    Added Note.kt. Used to store note info in the database.

---
+++ app/src/main/java/com/raineru/panatilihin2/data/Note.kt
@@ -0,0 +1,15 @@
+package com.raineru.panatilihin2.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "notes"
+)
+data class Note(
+    val title: String,
+    val content: String,
+
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0
+)
