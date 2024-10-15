09c605aec7fa75e1585b383d70475de8e5549117
rynel <ranajirune@gmail.com>
2024-10-16 03:20:11 +0800

    Added support for filtering the notes with the searchQuery field.

--- app/src/main/java/com/raineru/panatilihin2/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/raineru/panatilihin2/ui/viewmodel/HomeViewModel.kt
@@ -1,3 +1,5 @@
+@file:OptIn(FlowPreview::class)
+
 package com.raineru.panatilihin2.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
@@ -5,8 +7,14 @@ import androidx.lifecycle.viewModelScope
 import com.raineru.panatilihin2.data.Note
 import com.raineru.panatilihin2.data.NoteRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
@@ -15,10 +23,36 @@ class HomeViewModel @Inject constructor(
     noteRepository: NoteRepository
 ) : ViewModel() {
 
+    private val _searchQuery = MutableStateFlow("")
+    val searchQuery: StateFlow<String> = _searchQuery.asStateFlow()
+
     val notes: StateFlow<List<Note>> = noteRepository.getAllNotes()
         .stateIn(
             viewModelScope,
             initialValue = emptyList(),
             started = SharingStarted.WhileSubscribed(5000)
         )
+        .combine(_searchQuery) { notes, query ->
+            Pair(notes, query)
+        }
+        .debounce(500)
+        .map { (users, query) ->
+            if (query.isEmpty()) {
+                users
+            } else{
+                users.filter {
+                    it.title.contains(query, ignoreCase = true) ||
+                            it.content.contains(query, ignoreCase = true)
+                }
+            }
+        }
+        .stateIn(
+            viewModelScope,
+            initialValue = emptyList(),
+            started = SharingStarted.WhileSubscribed(5000)
+        )
+
+    fun updateSearchQuery(query: String) {
+        _searchQuery.value = query
+    }
 }
