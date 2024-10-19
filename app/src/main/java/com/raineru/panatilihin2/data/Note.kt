518d348fd001b85afea37687ff3106f7db167716
rynel <ranajirune@gmail.com>
2024-10-19 19:21:34 +0800

    Added Serializable annotation to be able to be passed as an argument in Navigation Compose.

--- app/src/main/java/com/raineru/panatilihin2/data/Note.kt
+++ app/src/main/java/com/raineru/panatilihin2/data/Note.kt
@@ -2,7 +2,9 @@ package com.raineru.panatilihin2.data
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.serialization.Serializable
 
+@Serializable
 @Entity(
     tableName = "notes"
 )
