7b4dcc5486c8e908185ddedec346503427b4dccf
rynel <ranajirune@gmail.com>
2024-10-20 16:58:38 +0800

    Updated MainActivity to implement core app functionality, including basic user interactions and task handling.

--- app/src/main/java/com/raineru/panatilihin/MainActivity.kt
+++ app/src/main/java/com/raineru/panatilihin/MainActivity.kt
@@ -3,44 +3,65 @@ package com.raineru.panatilihin
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.raineru.panatilihin.ui.theme.PanatilihinTheme
+import com.raineru.panatilihin2.data.Note
+import com.raineru.panatilihin2.navigation.HomeSubNavGraph
+import com.raineru.panatilihin2.navigation.homeScreen
+import com.raineru.panatilihin2.navigation.homeSubNavGraph
+import com.raineru.panatilihin2.screen.NoteScreen
+import com.raineru.panatilihin2.ui.viewmodel.NoteViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Start test code -----------------------------------------------
+//        enableEdgeToEdge()
+
         setContent {
             PanatilihinTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                val navController = rememberNavController()
+
+                NavHost(
+                    navController = navController,
+                    startDestination = HomeSubNavGraph
                 ) {
-                    Greeting("Android")
+                    homeSubNavGraph {
+                        homeScreen(navController)
+
+                        composable<Note> { backStackEntry ->
+                            val note: Note = backStackEntry.toRoute()
+
+                            val noteViewModel: NoteViewModel =
+                                hiltViewModel<NoteViewModel, NoteViewModel.Factory>(
+                                    creationCallback = { factory ->
+                                        factory.create(
+                                            title = note.title,
+                                            content = note.content,
+                                            id = note.id
+                                        )
+                                    }
+                                )
+
+                            NoteScreen(
+                                titleTextFieldState = noteViewModel.noteTitleState,
+                                contentTextFieldState = noteViewModel.noteContentState,
+                                onBack = {
+                                    navController.navigateUp()
+                                }
+                            )
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PanatilihinTheme {
-        Greeting("Android")
     }
 }
